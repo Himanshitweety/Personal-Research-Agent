678625eb2cbd69f1ebb2a77290493d04b93348b9
Himanshi Singh <himanshitwettysingh@gmail.com>
2026-09-24 00:21:59 +0530

    Added functionalities

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 # 0
- 
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+
+load_dotenv()  # reads your .env file so GROQ_API_KEY is available
+from langchain_core.messages import AIMessage ,SystemMessage , HumanMessage
+llm = ChatGroq(model="openai/gpt-oss-20b" , temperature=0.9)
+
+messages = [
+    SystemMessage(content="You are Funny Ai agent")
+]
+
+
+print("--------------Welcome type 0 to exit the application------------- ")
+while True:
+    prompt = input("you : ")
+    messages.append(HumanMessage(content=prompt))
+   
+    if prompt == "0":
+        break
+    response = llm.invoke(messages)
+    messages.append(AIMessage(content=response.content))
+
+    print("Bot :", response.content)
+
+print(messages)
